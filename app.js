3c71f1b6fef6fe30c55883b4195be69fc2a1a0ef
Izzytraining <izzyjhalliday23@gmail.com>
2022-12-13 20:33:04 +0000

    "changes"

---
+++ app.js
@@ -0,0 +1,46 @@
+//task1 Breakfast
+
+const breakfast = ["toast", "sausage", "eggs", "orange juice"];
+console.log(breakfast);
+
+breakfast[2] = "beans";
+console.log("change array", breakfast);
+
+breakfast.push("eggs");
+console.log(breakfast);
+
+breakfast.unshift("bacon");
+console.log(breakfast);
+
+console.log(breakfast.length);
+
+//task 2 Last Value
+coins = [1, 2, 3, 4, 5];
+const lastcoins = coins.slice(-1);
+console.log(lastcoins);
+
+//task 3 Music
+
+const musicGenres = ["Rock", "Rap"];
+musicGenres.push("Jazz");
+console.log(musicGenres);
+
+musicGenres[2] = "Classical";
+console.log("change array", musicGenres);
+
+musicGenres.unshift("Blues", "Reggae");
+console.log(musicGenres);
+
+//task 4
+
+function sort
+
+
+//task Loop 1
+
+//task Loop 2
+
+
+
+
+
