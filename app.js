03d4c48e2519ad7265355d8f88430ea12541a430
Izzytraining <izzyjhalliday23@gmail.com>
2022-12-15 11:25:50 +0000

    addedtask

--- app.js
+++ app.js
@@ -38,8 +38,37 @@ console.log(breakfast);
 
 //task Loop 1 Counting
 
-for (let i = 1; (i = 10); i++) {
+for (let i = 1; (i = 10); i--) {
   console.log(i); // printing the value of i
 }
+
 //task Loop 2 tasks
-const tasks = ["hoover", "tidy", "cook"];
+//const tasks = ["hoover", "tidy", "cook"];for ()
+//1. Create an array, called tasks, of 3 or more household tasks you
+//need to do.
+//2.
+//Use a for loop and template literals to iterate through the
+//array printing 'Task $(1+1}: S(taskslil)" for each element on the
+//array.
+
+//task loop 3 Last loop value
+let i = 3;
+while (i) {
+  alert(i--);
+}
+console.console.log("value of i", i);
+
+//task loop 4 Even numbers
+for (let i = 2; i <= 10; i += 2) {
+  console.log("Count", i);
+}
+
+//task ForEach Exercise 1: Fav Foods
+const favouriteFood =
+  ("chocolate", "roast potatoes", "chilli chicken noodles", "bacon roll");
+function iterate(item) {
+  console.log(item);
+}
+favouriteFood.forEach(iterate);
+
+//task
